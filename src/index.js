4f8948f56842e0cf9c7a42e51f4e102cbd4b4201
Avinash P Nishad <avinash.nishah.sa@gmail.com>
2018-07-28 21:27:45 -0700

    init react start UP
    React start UP staructure

---
+++ src/index.js
@@ -0,0 +1,9 @@
+//react api 
+import React from "react";
+import ReactDOM from "react-dom";
+
+//Store
+ReactDOM.render(
+  <h1>React Basic Start UP Structure</h1>,
+  document.querySelector(".container")
+);
